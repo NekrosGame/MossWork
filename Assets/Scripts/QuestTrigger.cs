fc06461e586edcc9c3023c43dccaf5eab74a91de
SeungIl <138782327+NekrosGame@users.noreply.github.com>
2025-07-25 20:07:33 +0900

    (update) 플레이어와 npc상호작용
    충돌 범위안에 있을경우 판넬 트리거 활성화

---
+++ Assets/Scripts/QuestTrigger.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class QuestTrigger : MonoBehaviour
+{
+    public GameObject rKeyPanel; // R키 패널 (활성/비활성 대상)
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Npc"))
+        {
+            rKeyPanel.SetActive(true);
+            Debug.Log("퀘스트 Npc 범위에 진입!");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Npc"))
+        {
+            rKeyPanel.SetActive(false);
+            Debug.Log("퀘스트 Npc 범위에서 이탈!");
+        }
+    }
+}
